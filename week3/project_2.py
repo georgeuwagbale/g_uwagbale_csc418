939b838015945f76c459128e9a08a4acb1ff2806
georgeuwagbale <uwagbalegeorge@gmail.com>
2024-04-03 10:37:09 +0100

    Lecture 4

---
+++ week3/project_2.py
@@ -0,0 +1,36 @@
+import tkinter as tk
+from tkinter import Button
+from tkinter import Entry
+
+user_database = [
+    {"username": "uwagbalegeorge", "email": "uwagbalegeorge@gmail.com", "password": "password", "age": 22}
+]
+
+window = tk.Tk()
+window.title("My First GUI Program")
+# window.geometry("400x400")
+window.resizable(False, False)
+
+tk.Label(window, text="Username").grid(row=1, column=0)
+username = Entry(window, width=30)
+username.grid(row=1, column=1)
+
+tk.Label(window, text="Password").grid(row=2, column=0)
+password = Entry(window, width=30)
+password.grid(row=2, column=1)
+
+
+def login() -> bool:
+    # print(username, password_)
+    for user in user_database:
+        if user.get("username") == username.get() and user.get("password") == password.get():
+            print("user logged in successfully!!")
+            return True
+
+    print("Incorrect email or password")
+    return False
+
+
+Button(window, text="Login", width=30, command=login).grid(row=3, column=1)
+
+window.mainloop()
